49bdf338f3ab0a9c98f865417a05c1d1a27978b5
chenziwen <401030526@qq.com>
2020-09-15 10:58:10 +0800

    1.创建文件，实现分片上传文件功能

---
+++ split-file-upload/src/main/java/com/czw/splitfileupload/SplitFileUploadApplication.java
@@ -0,0 +1,13 @@
+package com.czw.splitfileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitFileUploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplitFileUploadApplication.class, args);
+    }
+
+}
---
+++ split-file-upload/src/test/java/com/czw/splitfileupload/SplitFileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czw.splitfileupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitFileUploadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
